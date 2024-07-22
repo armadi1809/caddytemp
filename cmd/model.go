4d46e5982ba69db3ccc443a5c58e4e3f60f3d96b
armadi1809 <azizrmadi@gmail.com>
2024-07-21 20:09:14 -0500

    POC switching to new page for placeholders input

--- cmd/model.go
+++ cmd/model.go
@@ -1,14 +1,13 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -22,6 +21,10 @@ var (
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	focusedStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle       = focusedStyle
+	noStyle           = lipgloss.NewStyle()
 )
 
 type item string
@@ -52,9 +55,12 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 }
 
 type model struct {
-	list     list.Model
-	choice   string
-	quitting bool
+	list             list.Model
+	choice           string
+	quitting         bool
+	step             int
+	selectedTemplate string
+	inputs           []textinput.Model
 }
 
 func (m model) Init() tea.Cmd {
@@ -74,54 +80,100 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 
 		case "enter":
-			i, ok := m.list.SelectedItem().(item)
-			if ok {
-				fileContent, ok := templates[string(i)]
-				if !ok {
-					m.choice = "An unexpected error occurred."
-					return m, tea.Quit
-				}
-				f, err := os.Create("Caddyfile")
-				if err != nil {
-					m.choice = "An unexpected error occurred."
-					return m, tea.Quit
-				}
-				defer f.Close()
-				userInputs := []any{}
-				for _, placeholder := range placeholdersData[string(i)] {
-					fmt.Printf("Enter %s: ", placeholder)
-					reader := bufio.NewReader(os.Stdin)
-					line, err := reader.ReadString('\n')
-					if err != nil {
-						log.Fatal(err)
+			switch m.step {
+			case 0:
+				i, ok := m.list.SelectedItem().(item)
+				if ok {
+					m.selectedTemplate = string(i)
+					placeholders := placeholdersData[string(i)]
+					m.inputs = make([]textinput.Model, len(placeholders))
+					var t textinput.Model
+					for i := range m.inputs {
+						t = textinput.New()
+						t.Cursor.Style = cursorStyle
+						t.CharLimit = 32
+						t.Placeholder = placeholders[i]
+						if i == 0 {
+							t.Focus()
+							t.PromptStyle = focusedStyle
+							t.TextStyle = focusedStyle
+						}
+						m.inputs[i] = t
 					}
-					userInputs = append(userInputs, line)
+					m.step += 1
 				}
-				_, err = f.WriteString(fmt.Sprintf(fileContent, userInputs...))
-				if err != nil {
-					m.choice = "An unexpected error occurred."
-					return m, tea.Quit
-				}
-				f.Sync()
-				m.choice = string(i)
+				return m, nil
+			case 1:
+				return m, m.updateInputs(msg)
+				// fileContent, ok := templates[m.selectedTemplate]
+				// if !ok {
+				// 	m.choice = "An unexpected error occurred."
+				// 	return m, tea.Quit
+				// }
+				// f, err := os.Create("Caddyfile")
+				// if err != nil {
+				// 	m.choice = "An unexpected error occurred."
+				// 	return m, tea.Quit
+				// }
+				// defer f.Close()
+				// userInputs := []any{}
+				// for _, placeholder := range placeholdersData[m.selectedTemplate] {
+				// 	fmt.Printf("Enter %s: ", placeholder)
+				// 	reader := bufio.NewReader(os.Stdin)
+				// 	line, err := reader.ReadString('\n')
+				// 	if err != nil {
+				// 		log.Fatal(err)
+				// 	}
+				// 	userInputs = append(userInputs, line)
+				// }
+				// _, err = f.WriteString(fmt.Sprintf(fileContent, userInputs...))
+				// if err != nil {
+				// 	m.choice = "An unexpected error occurred."
+				// 	return m, tea.Quit
+				// }
+				// f.Sync()
 			}
+
 			return m, tea.Quit
 		}
 	}
-
+	if m.step == 1 {
+		return m, m.updateInputs(msg)
+	}
 	var cmd tea.Cmd
 	m.list, cmd = m.list.Update(msg)
 	return m, cmd
 }
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
 
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
 func (m model) View() string {
+	if m.step == 0 {
+		return "\n" + m.list.View()
+	}
 	if m.choice != "" {
 		return quitTextStyle.Render(fmt.Sprintf("Generating Caddyfile: %s", m.choice))
 	}
 	if m.quitting {
 		return quitTextStyle.Render("Quitting...")
 	}
-	return "\n" + m.list.View()
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+	return b.String()
 }
 
 func run() {
@@ -138,7 +190,7 @@ func run() {
 	l.Styles.PaginationStyle = paginationStyle
 	l.Styles.HelpStyle = helpStyle
 
-	m := model{list: l}
+	m := model{list: l, step: 0}
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
